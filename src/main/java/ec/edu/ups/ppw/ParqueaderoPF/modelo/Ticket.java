0b3a2fbc1d09e822381f96719828c324ea9d79a6
mejiamagna <mmejias1@est.ups.edu.ec>
2023-08-02 12:43:32 -0500

    servicioTicket

--- src/main/java/ec/edu/ups/ppw/ParqueaderoPF/modelo/Ticket.java
+++ src/main/java/ec/edu/ups/ppw/ParqueaderoPF/modelo/Ticket.java
@@ -2,59 +2,82 @@ package ec.edu.ups.ppw.ParqueaderoPF.modelo;
 
 import java.util.Date;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Ticket {
-@Id
-@GeneratedValue
-@Column(name="tic_id")
-private int id;
+	
+	@Id
+	@GeneratedValue
+	@Column(name="tic_id")
+	private int id;
 
-@Column(name="tic_fecha")
-private Date fecha;
+	@Column(name="tic_numero")
+	private int numeroTicket;
 
-@Column(name="tic_puesto")
-private int puesto;
+	@Column(name="tic_puesto")
+	private int puesto;
 
-@Column(name="tic_hora_entrada")
-private Date horaEntrada;
+	@Column(name="tic_hora_entrada")
+	private Date horaEntrada;
 
-@Column(name="tic_hora_salida")
-private Date horaSalida;
+	@Column(name="tic_hora_salida")
+	private Date horaSalida;
 
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public Date getFecha() {
-	return fecha;
-}
-public void setFecha(Date fecha) {
-	this.fecha = fecha;
-}
-public int getPuesto() {
-	return puesto;
-}
-public void setPuesto(int puesto) {
-	this.puesto = puesto;
-}
-public Date getHoraEntrada() {
-	return horaEntrada;
-}
-public void setHoraEntrada(Date horaEntrada) {
-	this.horaEntrada = horaEntrada;
-}
-public Date getHoraSalida() {
-	return horaSalida;
-}
-public void setHoraSalida(Date horaSalida) {
-	this.horaSalida = horaSalida;
-}
+	//relaciones
+	@OneToOne
+    @JoinColumn(name = "veh_placa")
+	private Vehiculo vehiculo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumeroTicket() {
+		return numeroTicket;
+	}
+	public void setNumeroTicket(int numeroTicket) {
+		this.numeroTicket = numeroTicket;
+	}
+	
+	public int getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(int puesto) {
+		this.puesto = puesto;
+	}
+	public Date getHoraEntrada() {
+		return horaEntrada;
+	}
+	public void setHoraEntrada(Date horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+	public Date getHoraSalida() {
+		return horaSalida;
+	}
+	public void setHoraSalida(Date horaSalida) {
+		this.horaSalida = horaSalida;
+	}
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", numeroTicket=" + numeroTicket + ", puesto=" + puesto
+				+ ", horaEntrada=" + horaEntrada + ", horaSalida=" + horaSalida + ", vehiculo=" + vehiculo + "]";
+	}
 
 }
--- src/main/java/ec/edu/ups/ppw/ParqueaderoPF/modelo/Vehiculo.java
+++ src/main/java/ec/edu/ups/ppw/ParqueaderoPF/modelo/Vehiculo.java
@@ -2,6 +2,9 @@ package ec.edu.ups.ppw.ParqueaderoPF.modelo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -9,57 +12,39 @@ import jakarta.persistence.OneToOne;
 @Entity
 public class Vehiculo {
 	
-@Id
-@Column(name="veh_placa")
-private String placa;
+	@Id
+	@Column(name="veh_placa")
+	private String placa;
 
-@Column(name="veh_marca")
-private String marca;
-<<<<<<< HEAD
+	@Column(name="veh_marca")
+	private String marca;
 
-=======
->>>>>>> fd5b9b8ee5ad9850dd5daa7b9279cc4fd5e6eef0
-@Column(name="veh_tipo")
-private String tipo;
+	@Column(name="veh_tipo")
+	private String tipo;
+	
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 
-//relaciones
-@OneToOne
-@JoinColumn(name="tic_id")
-private Ticket ticket;
-
-public String getPlaca() {
-	return placa;
-}
-public void setPlaca(String placa) {
-	this.placa = placa;
-}
-public String getMarca() {
-	return marca;
-}
-public void setMarca(String marca) {
-	this.marca = marca;
-}
-public String getTipo() {
-	return tipo;
-}
-public void setTipo(String tipo) {
-	this.tipo = tipo;
-}
-
-<<<<<<< HEAD
-public Ticket getTicket() {
-	return ticket;
-}
-public void setTicket(Ticket ticket) {
-	this.ticket = ticket;
-}
-@Override
-public String toString() {
-	return "Vehiculo [placa=" + placa + ", marca=" + marca + ", tipo=" + tipo + ", ticket=" + ticket + "]";
-}
-
-=======
->>>>>>> fd5b9b8ee5ad9850dd5daa7b9279cc4fd5e6eef0
+	@Override
+	public String toString() {
+		return "Vehiculo [placa=" + placa + ", marca=" + marca + ", tipo=" + tipo + "]";
+	}
 
 }
 
